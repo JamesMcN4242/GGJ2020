315aa4b23129cd3efa46e5d0e28cb12d1fa6b6b2
jamesmcn4242 <jamesmcneil42@hotmail.co.uk>
2020-02-01 15:38:11 +0000

    Game UI script created

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    [SerializeField]
+    private Text[] m_playerScoreTexts = new Text[2];
+
+    [SerializeField]
+    private Text m_timerText = null;
+
+    public void UpdatePlayerScore(int playerIndex, int score)    
+    {
+        m_playerScoreTexts[playerIndex].text = string.Format("Player {0} Score: {1}", playerIndex, score);
+    }
+
+    public void UpdateGameTimer(int secondsRemaining)
+    {
+        m_timerText.text = string.Format("Time Remaining: {0}s", secondsRemaining);
+    }
+}
